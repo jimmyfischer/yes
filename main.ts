bcddc392b67d72622c45ae2a458bf372bd977f3c
jimmyfischer <93795791+jimmyfischer@users.noreply.github.com>
2021-11-05 13:46:28 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let level = 1
+if (true) {
+    tiles.setTilemap(tilemap`level15`)
+}
+if (true) {
+    tiles.setTilemap(tilemap`level15`)
+}
+if (true) {
+    tiles.setTilemap(tilemap`level15`)
+}
+if (true) {
+    tiles.setTilemap(tilemap`level15`)
+}
+if (true) {
+    tiles.setTilemap(tilemap`level15`)
+}
+if (true) {
+    tiles.setTilemap(tilemap`level15`)
+}
